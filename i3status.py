cacd3ade02bb2bc8ceba742c7814f54489d2c397
Lineu Felipe <lineufelipe@gmail.com>
2019-05-15 18:23:46 -0300

    Update status bar

--- i3status.py
+++ i3status.py
@@ -1,6 +1,22 @@
+import os
 from i3pystatus import Status
 
 status = Status()
 
+status.register('clock',
+                format='%d/%m/%Y %H:%M',)
+
+status.register('load',
+                format='{avg1}')
+
+status.register('disk',
+                path='/',
+                format='{percentage_avail}%')
+
+hostname = os.uname()[1]
+if hostname == 'lineufelipe.ws':
+    status.register('network',
+                    interface='enp3s0',
+                    format_up='{interface} {v4cidr}')
 
 status.run()
